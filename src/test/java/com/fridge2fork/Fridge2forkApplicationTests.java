58dde0992e25aea189835255de6c5771bb9ca895
hi4579675 <hi4579675@gmail.com>
2026-03-24 11:26:22 +0900

    chore: initial project setup

---
+++ src/test/java/com/fridge2fork/Fridge2forkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fridge2fork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fridge2forkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
