6b1e7ebdab1646240724af1db66281f7fbdba6bf
hi4579675 <hi4579675@gmail.com>
2026-03-26 09:36:27 +0900

    feat: RedisConfig, SwaggerConfig 작성

--- src/main/java/com/fridge2fork/global/config/SwaggerConfig.java
+++ src/main/java/com/fridge2fork/global/config/SwaggerConfig.java
@@ -1,4 +1,37 @@
 package com.fridge2fork.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    private static final String BEARER_AUTH = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(BEARER_AUTH);
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fridge2Fork API")
+                        .description("Fridge2Fork 서비스 API 문서")
+                        .version("v1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, securityScheme));
+    }
 }
